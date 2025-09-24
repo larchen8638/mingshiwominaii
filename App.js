937938a60d8865b826bd4a16f4158cabde6a9955
larchen8638 <jinlong8638642@gmail.com>
2025-09-24 11:44:13 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import UserDetail from './pages/UserDetail';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<UserDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
